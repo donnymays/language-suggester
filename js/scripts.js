26d228a7b36d2d11a145256f2d51a3e8e666e385
donnymays <dtmays2@gmail.com>
2020-08-14 09:39:39 -0700

    add name input js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#questions").submit(function() {
     event.preventDefault();
     let language = parseInt($("#interests").val());
-    let name = $("input#name").val();
+    let nameInput = $("input#name").val();
   
     if (language === 1) {
       $("#css").show();
@@ -17,6 +17,7 @@ $(document).ready(function() {
       $("#css").hide();
       $("#swift").hide();
     };
-  
+    $(".name").text(nameInput);
+
   });
 });
