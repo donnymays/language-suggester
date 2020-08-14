bea77541fc8eedddc8500b7563007d990b6ad5ca
donnymays <dtmays2@gmail.com>
2020-08-14 09:00:31 -0700

    create language and name variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("form#questions").submit(function() {
-
+    event.preventDefault();
+    const language = parseInt($("#interests").val());
+    const name = $("input#name").val();
   });
 });
