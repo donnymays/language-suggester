4e47e93bb00933f4c766e131c79e8ba7a8d39f01
donnymays <dtmays2@gmail.com>
2020-08-14 12:30:18 -0700

    create new branch and add radio button question and js - it worksgit add .git add .git add .git add .

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,29 @@ $(document).ready(function() {
     event.preventDefault();
     let language = parseInt($("#interests").val());
     let nameInput = $("input#name").val();
+    let mobileInput = $("input:radio[name=mobile]:checked").val()
   
     if (language === 1) {
-      $("#css").fadeIn();
+      $("#css").show();
       $("#swift").hide();
       $("#ruby").hide();
-    } else if ( language === 2) {
+    } else if ( language === 2 && mobileInput === "ios") {
       $("#swift").show();
       $("#css").hide();
       $("#ruby").hide();
-    } else {
+      $("#c-sharp").hide();
+    } else if ( language === 2 && mobileInput === "android") {
+      $("#swift").hide();
+      $("#css").hide();
+      $("#ruby").hide();
+      $("#c-sharp").show();
+    }else {
       $("#ruby").show();
       $("#css").hide();
       $("#swift").hide();
     };
+    
     $(".name").text(nameInput);
-
     if (nameInput != "") {
       $("answer").show();
     } else {
